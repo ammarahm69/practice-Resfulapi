76280e96f397cbf6d29eb39cdb2e919e005c436c
Ammar Ahmed <ammar69aa59@gmail.com>
2024-06-01 23:41:44 -0700

    data showed

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 async function getData() {
     try {
-        let arr = [];
-        let response = await fetch('https://fakestoreapi.com/products')
-        let data = await response.json()
-        arr.push(...data)
+        let response = await fetch('https://fakestoreapi.com/products');
+        let cards = document.getElementById('card');
+        let data = await response.json();
 
-        let transformedData = arr.map((products) => {
+        let transformedData = data.map((product) => {
             return {
-                title: products.title,
-                price: products.price,
-                description: products.description.toUpperCase()
-            }
-        })
-        transformedData.forEach(element => {
+                title: product.title,
+                image: product.image,
+                price: product.price,
+                description: product.description.toUpperCase().slice(0,20)
+            };
         });
-        
-          let filteredData = arr.filter(products => products.price > 50);
-          console.log('Filtered Data:', filteredData);
 
-    }
-    catch (err) {
+        transformedData.forEach(product => {
+            cards.innerHTML += `
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-5">
+                    <div class="card" style="width: 18rem;">
+                        <img src="${product.image}" class="card-img-top mb-2 p-2" alt="${product.title}">
+                        <div class="card-body">
+                            <h5 class="card-title">${product.title}</h5>
+                            <p class="card-text">${product.description}</p>
+                            <p class="card-text"><strong>Price</strong>: $${product.price}</p>
+                        </div>
+                    </div>
+                </div>
+            `;
+        });
+    } catch (err) {
         console.log('Fetching Error', err);
     }
 }
-getData()
+
+getData();
